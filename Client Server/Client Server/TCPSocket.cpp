76765ec4ef52a083bceeac0f45a4f06241887b56
zachChilders <zwc101@gmail.com>
2014-10-28 16:24:08 -0700

    Send String

--- Client Server/Client Server/TCPSocket.cpp
+++ Client Server/Client Server/TCPSocket.cpp
@@ -13,7 +13,15 @@ tcp_error_t TCPSocket::recieveBytes()
 
 tcp_error_t TCPSocket::sendString(std::string msg)
 {
-
+	int res = send(connectSocket, msg.c_str(), msg.length(), 0);
+	if (res == SOCKET_ERROR)
+	{
+		std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(connectSocket);
+		WSACleanup();
+		return;
+	}
+	std::cout << "Bytes sent: " << res << std::endl;
 }
 
 tcp_error_t TCPSocket::recieveString()
