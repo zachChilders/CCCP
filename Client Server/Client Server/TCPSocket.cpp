000a21856473b467d7807fc22bed339927d8df95
zachChilders <zwc101@gmail.com>
2014-10-28 18:26:49 -0700

    Push the button

--- Client Server/Client Server/TCPSocket.cpp
+++ Client Server/Client Server/TCPSocket.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 tcp_error_t TCPSocket::sendBytes(vector<byte> data)
 {
-
+	return SUCCESS;
 }
 
 tcp_error_t TCPSocket::recieveBytes()
 {
-
+	return SUCCESS;
 }
 
 tcp_error_t TCPSocket::sendString(std::string msg)
@@ -19,7 +19,7 @@ tcp_error_t TCPSocket::sendString(std::string msg)
 		std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
 		closesocket(connectSocket);
 		WSACleanup();
-		return;
+		return CONNECTIONFAIL;
 	}
 	std::cout << "Bytes sent: " << res << std::endl;
 	return SUCCESS;
@@ -27,7 +27,7 @@ tcp_error_t TCPSocket::sendString(std::string msg)
 
 tcp_error_t TCPSocket::recieveString()
 {
-	int res = recv(connectSocket, recvbuf.get(), DEFAULT_BUFLEN, 0);
+	int res = recv(connectSocket, (char*) recvbuf.get(), DEFAULT_BUFLEN, 0);
 	if (res > 0)
 	{
 		return SUCCESS;
