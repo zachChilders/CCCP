b0e9ec1ae7beb669c93900fe17b67a18b47f8169
Tylor Lake <tylorlake@gmail.com>
2014-10-29 16:24:45 -0700

    Implemented quick zlib compression and decompression in C++

--- Client Server/Client Server/main.cpp
+++ Client Server/Client Server/main.cpp
@@ -4,42 +4,49 @@
 #include <stdlib.h>
 #include <zlib.h>
 
-int main() 
+int main()
 {
-
-
-	char *buf = new char[128];
-	int num_read = 0;
-
-	FILE *inFile = fopen("Output.txt", "rb");
-	gzFile out = gzopen("littleOutput.txt", "wb");
-	
-	while ((num_read = fread(buf, 1, 128, inFile)) > 0)
 	{
-		gzwrite(out, buf, num_read);
-	}
-	fclose(inFile);
-	gzclose(out);
+		std::ifstream infile("Output.txt", std::ios::binary);
+		gzFile outfile = gzopen("littleOutput.txt", "wb");
+		if (!infile || !outfile) return -1;
 
-	FILE *outFile = fopen("BigOutput.txt", "wb");
-	gzFile inCompress = gzopen("littleOutput.txt", "rb");
-	
-	while (num_read = gzread(inCompress, buf, 128) > 0)
-	{
-		fwrite(buf, 1, num_read, outFile);
+		char inbuffer[128];
+		int num_read = 0;
+		unsigned long total_read = 0, total_wrote = 0;
+		while (infile.read(inbuffer, 128).rdstate() == std::ios_base::goodbit) {
+			total_read += num_read;
+			gzwrite(outfile, inbuffer, infile.gcount());
+
+		}
+		infile.close();
+		gzclose(outfile);
 	}
-	fclose(outFile);
-	gzclose(inCompress);
+
+	{
+		gzFile infile = gzopen("littleOutput.txt", "rb");
+		std::ofstream outfile("enbiggenOutput.txt", std::ios::binary);
+		if (!infile || !outfile) return -1;
+
+		char buffer[128];
+		int num_read = 0;
+		while ((num_read = gzread(infile, buffer, sizeof(buffer))) > 0) {
+			outfile.write(buffer, num_read);
+		}
+
+		gzclose(infile);
+		outfile.close();
+	}
 
 	//Server Test 
 	/*TCPServer s;+
 	s.start();
-*/
+	*/
 	//Client Test
 	//TCPClient c("172.22.1.82");
 	//c.start();
 	//c.stop();
 
 	//s.stop();
-	
+
 }
