5410d7547ffa40d84f5a52d64cdd5059982b60ab
zachChilders <zwc101@gmail.com>
2014-10-29 14:28:28 -0700

    Now zlib works.

--- Client Server/Client Server/main.cpp
+++ Client Server/Client Server/main.cpp
@@ -1,14 +1,16 @@
 #include "TCPServer.hpp"
 #include "TCPClient.h"
-
+#include <zlib.h>
 
 int main() 
 {
+
+	std::cout << zlibVersion() << std::endl;
 	//Server Test 
 	TCPServer s;
 	s.start();
 
-	////Client Test
+	//Client Test
 	//TCPClient c("172.22.1.82");
 	//c.start();
 	//c.stop();
